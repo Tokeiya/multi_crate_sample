6e0d04a46f2d6e83af7b6af531ababc26a0ce375
tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2025-03-22 13:17:51 +0900

    Add basic test structures and update module configuration
    Added test modules to `main.rs` and `lib.rs`, including simple test cases. Updated IntelliJ project configuration to include source folders and exclude target directory for better project management.

--- libraly/src/lib.rs
+++ libraly/src/lib.rs
@@ -3,9 +3,16 @@ pub fn add(left: u64, right: u64) -> u64 {
 }
 
 #[cfg(test)]
+//When running in the windows.
+//Can running the test and debug.
+//But can't running with the coverage.
 mod tests {
     use super::*;
 
+
+    //When running in the windows.
+    //Can running the test and debug.
+    //But can't running with the coverage
     #[test]
     fn it_works() {
         let result = add(2, 2);
--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,3 +1,20 @@
+//main function can running with coverage.
 fn main() {
     println!("Hello, world!");
+}
+
+
+#[cfg(test)]
+
+//When running in the windows.
+//Can running the test and debug.
+//But can't running with the coverage.
+mod test{
+    //When running in the windows.
+    //Can running the test and debug.
+    //But can't running with the coverage.
+    #[test]
+    fn it_works(){
+        assert_eq!(2 + 2, 4);
+    }
 }
